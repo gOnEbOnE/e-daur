b614a09ab1d4eca7a0dec0d769eea0c55225148e
gOnEbOnE <proaielaeu@gmail.com>
2024-09-07 12:30:24 +0700

    menambahkan allowed host

--- e_daur/settings.py
+++ e_daur/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)^37h6hdqi+&1bu8$&3vltua()&cfv+%qtlsile&p(twz9o&ad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1",]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "christopher-matthew31-daurelektronik.pbp.cs.ui.ac.id"]
 
 
 # Application definition
